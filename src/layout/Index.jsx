cdb9e65362343d558e340c6440b1eaafe76b5a57
A-yadav07 <ayadav02072001@gmail.com>
2025-04-18 17:50:31 +0530

    save

--- src/layout/Index.jsx
+++ src/layout/Index.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import Home from "../pages/Home";
 import ContactPage from "../Component/Contact.jsx";
-import Comperhensive from "../component/comprohensive.jsx";
 import Posibility from "../component/Posibility.jsx";
 import Faq from "../component/Faq.jsx";
-// import AdminLayout from "../Component/layout/AdminLayout.jxs";
-
-import SaasAbleFigma from "../component/SaasAbleFigma.jsx";
 import SaasAbleBlocksPage from "../Component/Gallary.jsx"
-
-
 function Index() {
   return (
     <>
@@ -18,7 +12,7 @@ function Index() {
       {/* <TrustSection */}
        {/* <AboutPag /> */}
        <SaasAbleBlocksPage/>
-      <Comperhensive/>
+      // <Comperhensive/>
       <Posibility/>
       {/* <PowerfulAdminInterface/> */}
       {/* <SaasAbleFigma/> */}
