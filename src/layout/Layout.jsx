ce814665aa3bd2f551fd19d6d282cb4c9af84316
A-yadav07 <ayadav02072001@gmail.com>
2025-04-18 17:27:53 +0530

    foj

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,8 +3,9 @@ import { Outlet } from "react-router-dom";
 import FloatingActionButton from "../pages/FloatingActionButton.jsx";
 import { useEffect, useState } from "react";
 import { ArrowUp } from "lucide-react";
-import Navbar from "../component/Navbar.jsx";
+
 import Footer from "../Component/Footer.jsx";
+import Navbar from "../component/Navbar.jsx";
 
 const Layout = ({childern}) => {
   return (
